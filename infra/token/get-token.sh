4afc6d235c895f68a5b9e15ad55962e48401434e
Emerson Dantas Pereira <emerson.dantaspereira@hotmail.com>
2026-08-02 21:41:01 -0300

    feat: fix config kopenid, get and apply token

---
+++ infra/token/get-token.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+kubectl exec -it testcurl -- sh
+
+curl --location --request POST 'http://keycloak.iam/realms/bets/protocol/openid-connect/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'client_id=kong' \
+--data-urlencode 'grant_type=password' \
+--data-urlencode 'username=maria' \
+--data-urlencode 'password=maria' \
+--data-urlencode 'client_secret=yK7I2syjpc9gxuzEbvKBWqsnLyPt2HXU' \
+--data-urlencode 'scope=openid'
